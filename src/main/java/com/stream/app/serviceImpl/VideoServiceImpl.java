40717944d57ea2ec0d4c95086888364ac63809ad
Harsh Pandey <hars30896@gmail.com>
2024-08-13 21:13:18 +0530

    minor add on

--- src/main/java/com/stream/app/serviceImpl/VideoServiceImpl.java
+++ src/main/java/com/stream/app/serviceImpl/VideoServiceImpl.java
@@ -1,15 +1,71 @@
 package com.stream.app.serviceImpl;
 
 import com.stream.app.entity.Video;
+import com.stream.app.repositories.VideoRepositories;
 import com.stream.app.service.VideoService;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.List;
 @Service
 public class VideoServiceImpl implements VideoService {
+
+    @Value("${files.video}")
+    String DIR;
+
+    @Autowired
+    private VideoRepositories videoRepositories;
+
+    @PostConstruct
+    public void init(){
+        File file = new File(DIR);
+        if(!file.exists()){
+            file.mkdir();
+            System.out.println("Folder created");
+        }else{
+            System.out.println("Folder already created");
+        }
+    }
+
     @Override
     public Video save(Video video, MultipartFile file) {
-        return null;
+        try {
+            // original FileName
+            String filename = file.getOriginalFilename();
+            String contentType = file.getContentType();
+            InputStream inputStream = file.getInputStream();
+
+            // File path: create
+            String cleanFileName = StringUtils.cleanPath(filename);
+            // folder path: create
+            String cleanFolder=StringUtils.cleanPath(DIR);
+
+            Path path = Paths.get(cleanFolder,cleanFileName);
+            System.out.println(path);
+            // copy file to the folder
+            Files.copy(inputStream,path, StandardCopyOption.REPLACE_EXISTING);
+
+            // create video metadata save
+            video.setContentType(contentType);
+            video.setFilePath(path.toString());
+            return videoRepositories.save(video);
+
+        }catch (Exception e){
+            e.printStackTrace();
+            return null;
+        }
+
     }
 
     @Override
